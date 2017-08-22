317a8ce67be207f7a12f19554c4664e03b129892
870314540 <870314540@qq.com>
2017-08-22 17:51:27 +0800

    return 的验证

---
+++ securitydemo/src/main/java/com/example/demo/onetest/ReturnTest.java
@@ -0,0 +1,37 @@
+package com.example.demo.onetest;
+
+/**
+ * Created by lenovo on 2017/8/22.
+ */
+public class ReturnTest {
+
+
+    public static int func(int arg){
+
+        int i   = 0;
+
+        try {
+            System.out.println("in try……");
+            i = 1000 / arg ;
+            //return i ;
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("in catching ……");
+            return 1000;
+        }finally {
+            System.out.println("in finally ……");
+           // return  10000;
+        }
+
+        return i;
+
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println(ReturnTest.func(0));
+
+
+    }
+}
