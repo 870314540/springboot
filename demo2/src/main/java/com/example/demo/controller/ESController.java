e31a45958c8db8d859fc1e25c3872e995abfc33f
cuiyt <cuiyt@jiedaibao.com>
2018-07-17 20:15:19 +0800

    321

--- demo2/src/main/java/com/example/demo/controller/ESController.java
+++ demo2/src/main/java/com/example/demo/controller/ESController.java
@@ -4,9 +4,7 @@ import com.example.demo.es.ESService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author cuiyt
@@ -27,9 +25,28 @@ public class ESController {
         return myname;
     }
 
-
     @PostMapping("/getResult")
-    public @ResponseBody String getResult() {
+    public @ResponseBody
+    String getResult() {
         return esService.getResult();
     }
+
+    @PostMapping("/getResultWithParam")
+    public @ResponseBody
+    String getResultWithParam(@RequestParam("index") String index,@RequestParam("type")  String type, @RequestParam("id")  String id) {
+        return esService.getResult(index, type, id);
+    }
+
+    @RequestMapping(value="/{index}/{type}/{id}",method= RequestMethod.GET)
+    public @ResponseBody
+    String getResultWithpath(@PathVariable("index") String index,@PathVariable("type")  String type, @PathVariable("id")  String id) {
+        return esService.getResult(index, type, id);
+    }
+    @PostMapping("/getResultWithIndex")
+    public @ResponseBody
+    String getResultWithIndex(@RequestParam("index") String index ) {
+        return esService.getResult(index);
+    }
+
+
 }
--- demo2/src/main/java/com/example/demo/es/ESService.java
+++ demo2/src/main/java/com/example/demo/es/ESService.java
@@ -2,6 +2,8 @@ package com.example.demo.es;
 
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,34 @@ import java.io.IOException;
  */
 @Service
 public class ESService {
+    private static final Logger logger = LoggerFactory.getLogger(ESService.class);
+
     @Autowired
     ElasticsearchConfiguration elasticsearchConfiguration;
 
+    //获取一个固定的结果
     public String getResult() {
         GetRequest request = new GetRequest("movies", "movie", "5");
+        return esResult(request);
+    }
+
+    //传入参数获取结果
+    public String getResult(String index, String type, String id) {
+        logger.info("index:" + index + ",type:" + type + ",id:" + id);
+        GetRequest request = new GetRequest(index, type, id);
+        return esResult(request);
+    }
+
+    //获取某个索引下的全部结果
+    public String getResult(String index) {
+        logger.info("index:" + index);
+        GetRequest request = new GetRequest(index);
+//        GetRequest requestAll = new GetRequest(); 获取全部的es数据
+        return esResult(request);
+    }
+
+    //请求
+    private String esResult(GetRequest request) {
         try {
             GetResponse response = elasticsearchConfiguration.getObject().get(request);
             System.out.println(response);
@@ -30,4 +55,6 @@ public class ESService {
         }
         return null;
     }
+
+
 }
