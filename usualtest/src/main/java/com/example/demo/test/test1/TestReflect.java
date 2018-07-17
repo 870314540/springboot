2507ac0d3f5638052ecb1e8d28b8be8bb3077f11
cuiyt <cuiyt@jiedaibao.com>
2018-07-17 14:51:09 +0800

    reflect

--- usualtest/src/main/java/com/example/demo/test/test1/TestReflect.java
+++ usualtest/src/main/java/com/example/demo/test/test1/TestReflect.java
@@ -1,5 +1,6 @@
 package com.example.demo.test.test1;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -15,29 +16,22 @@ public class TestReflect {
         UserEntity userEntity = (UserEntity) userClass.newInstance();
 
         //第一种方法
-        System.out.println("第一次借钱：");
         int money = userEntity.getMoney();
-        System.out.println("实际拿到钱为: " + money);
-        System.out.println("------------------------分割线------------------------");
 
         //第二种方法,（无参的示例：借钱）
-        System.out.println("第二次借钱：");
         Method getMoney = userClass.getMethod("getMoney");//得到方法对象
         Object money2 = getMoney.invoke(userEntity);//调用借钱方法，得到返回值
         System.out.println("实际拿到钱为：" + money2);
-        System.out.println("------------------------分割线------------------------");
 
         //第二种方法,（单个参数的示例：还钱）
-        System.out.println("第一次还钱：");
         Method repay1 = userClass.getMethod("repay",int.class);//得到方法对象,有参的方法需要指定参数类型
         repay1.invoke(userEntity,3000);//执行还钱方法，有参传参
-        System.out.println("------------------------分割线------------------------");
 
-        //第二种方法,（单个参数的示例：还钱）
-        System.out.println("第二次还钱：");
+        //第二种方法,（多个参数的示例：还钱）
         Method repay2 = userClass.getMethod("repay", String.class,int.class);//得到方法对象,有参的方法需要指定参数类型
         repay2.invoke(userEntity,"小飞",5000);//执行还钱方法，有参传参
 
+        Field[] field=userClass.getDeclaredFields();
 
     }
 }
