20faa154aa273795acbdca98f5262433883fa842
cuiyt <cuiyt@jiedaibao.com>
2018-07-31 19:44:00 +0800

    1

--- usualtest/src/main/java/com/example/demo/test/test1/StringTest.java
+++ usualtest/src/main/java/com/example/demo/test/test1/StringTest.java
@@ -11,17 +11,16 @@ import java.util.*;
  */
 public class StringTest {
     public static void main(String[] args) {
-        Map<String,String> resultMap = new HashMap<String,String>();
-        String s1 = "2" ;
+        Map<String, String> resultMap = new HashMap<String, String>();
+        String s1 = "2";
         String appIds[] = new String[2];
         appIds[1] = "1";
         appIds[0] = s1;
 
 
-
-        String s2 = "[0,"+s1+"]";
-        resultMap.put("m1",JSON.toJSONString(appIds));
-        resultMap.put("m2",s2);
+        String s2 = "[0," + s1 + "]";
+        resultMap.put("m1", JSON.toJSONString(appIds));
+        resultMap.put("m2", s2);
 
         System.out.println(JSON.toJSONString(resultMap));
 
@@ -30,7 +29,7 @@ public class StringTest {
         System.out.println(resutl1.toString());
 
 
-        Map<String,Integer> resultMap1 = new LinkedHashMap<>();
+        Map<String, Integer> resultMap1 = new LinkedHashMap<>();
         System.out.println(resultMap1.get("321"));
 
         Set<String> nullSet = new HashSet<>();
@@ -38,6 +37,17 @@ public class StringTest {
         System.out.println(nullSet.contains("321"));
 
 
+        UserEntity entity = new UserEntity();
+        entity.setUserName("焦作/济源");
+        entity.setUserAge(19);
+        System.out.println(JSON.toJSONString(entity));
+
+        String name = entity.getUserName();
+        System.out.println(name);
+        if (name.contains("/")) {
+            name = name.split("\\/")[0];
+        }
+        System.out.println(name);
 
     }
 }
--- usualtest/src/main/java/com/example/demo/test/test2/TestBean.java
+++ usualtest/src/main/java/com/example/demo/test/test2/TestBean.java
@@ -41,5 +41,8 @@ public class TestBean {
         BeanUtils.copyProperties(bean11, bean3);
         System.out.println(JSON.toJSONString(bean3));
 
+
+
+
     }
 }
