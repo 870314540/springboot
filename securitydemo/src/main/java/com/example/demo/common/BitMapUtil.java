cd9beb1a74972d232af7b1febfbbfdae278bdd15
cuiyt <cuiyt@jiedaibao.com>
2018-12-03 17:42:09 +0800

    test

--- securitydemo/src/main/java/com/example/demo/common/BitMapUtil.java
+++ securitydemo/src/main/java/com/example/demo/common/BitMapUtil.java
@@ -1,9 +1,24 @@
 package com.example.demo.common;
 
 /**
- * @author cuiyt
+ * @author ThomasCui
  * @Descrption
  * @create 2018/11/13
  */
 public class BitMapUtil {
+    public static int returnActualLength(byte[] data) {
+        int i = 0;
+        for (; i < data.length; i++) {
+            if (data[i] == '\0')
+                break;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Byte[] bytes = {1, 2};
+        System.out.println(bytes.length);
+        byte[] bytes1 = {1, 2};
+        System.out.println(returnActualLength(bytes1) + "-->" + bytes1.length);
+    }
 }
--- securitydemo/src/main/java/com/example/demo/onetest/futuretest/Test1.java
+++ securitydemo/src/main/java/com/example/demo/onetest/futuretest/Test1.java
@@ -1,9 +1,51 @@
 package com.example.demo.onetest.futuretest;
 
+import java.util.concurrent.*;
+
 /**
  * @author cuiyt
  * @Descrption
  * @create 2018/12/3
  */
 public class Test1 {
+    public static void main(String[] args) {
+        /**
+         *   return new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+         *                                       60L, TimeUnit.SECONDS,
+         *                                       new SynchronousQueue<Runnable>())
+         */
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        Future<Integer> result = executor.submit(task);
+        executor.shutdown();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println("主线程在执行任务");
+
+        try {
+            System.out.println("task运行结果"+result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("所有任务执行完毕");
+    }
+}
+class Task implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("子线程在进行计算");
+        Thread.sleep(3000);
+        int sum = 0;
+        for(int i=0;i<100;i++)
+            sum += i;
+        return sum;
+    }
 }
