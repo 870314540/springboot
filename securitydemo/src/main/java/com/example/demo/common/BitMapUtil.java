c37069e25429693a32a07e499193aca497e79bd8
cuiyt <cuiyt@jiedaibao.com>
2018-12-03 17:40:49 +0800

    test

---
+++ securitydemo/src/main/java/com/example/demo/common/BitMapUtil.java
@@ -0,0 +1,9 @@
+package com.example.demo.common;
+
+/**
+ * @author cuiyt
+ * @Descrption
+ * @create 2018/11/13
+ */
+public class BitMapUtil {
+}
---
+++ securitydemo/src/main/java/com/example/demo/onetest/futuretest/Test1.java
@@ -0,0 +1,9 @@
+package com.example.demo.onetest.futuretest;
+
+/**
+ * @author cuiyt
+ * @Descrption
+ * @create 2018/12/3
+ */
+public class Test1 {
+}
