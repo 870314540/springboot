e59028b34e89dbf1df7ea8f7391908fa3ca2495d
cuiyt <cuiyt@jiedaibao.com>
2019-01-14 19:09:11 +0800

    test

--- securitydemo/src/main/java/com/example/demo/lucenetest/TestFST.java
+++ securitydemo/src/main/java/com/example/demo/lucenetest/TestFST.java
@@ -1,9 +1,61 @@
 package com.example.demo.lucenetest;
 
+
+import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.BytesRefBuilder;
+import org.apache.lucene.util.IntsRef;
+import org.apache.lucene.util.IntsRefBuilder;
+import org.apache.lucene.util.fst.*;
+
+import java.util.Comparator;
+
 /**
  * @author cuiyt
  * @Descrption
  * @create 2019/1/14
  */
 public class TestFST {
+
+    public static void main(String[] args) {
+//todo 待验证
+        String[] inputValues = {"abe", "abf", "ade"};
+        long outputValues[] = {5, 7, 12};
+
+        try {
+            PositiveIntOutputs outputs = PositiveIntOutputs.getSingleton();
+            Builder<Long> builder = new Builder<Long>(FST.INPUT_TYPE.BYTE1, outputs);
+            BytesRefBuilder scratchBytes = new BytesRefBuilder();
+            IntsRefBuilder scratchInts = new IntsRefBuilder();
+            for (int i = 0; i < inputValues.length; i++) {
+                scratchBytes.copyChars(inputValues[i]);
+                builder.add(Util.toIntsRef(scratchBytes.toBytesRef(), scratchInts), outputValues[i]);
+            }
+            FST<Long> fst = builder.finish();
+
+
+            Long value = Util.get(fst, new BytesRef("abf"));
+            System.out.println(value); // 7
+
+            IntsRef key = Util.getByOutput(fst, 12);
+            System.out.println(Util.toBytesRef(key, scratchBytes).utf8ToString()); // ade
+
+            BytesRefFSTEnum<Long> iterator = new BytesRefFSTEnum<Long>(fst);
+
+            while (iterator.next() != null) {
+                BytesRefFSTEnum.InputOutput<Long> mapEntry = iterator.current();
+                System.out.println(mapEntry.input.utf8ToString());
+                System.out.println(mapEntry.output);
+            }
+
+
+
+            Comparator<Long> comparator = new Comparator<Long>() {
+                public int compare(Long left, Long right) {
+                    return left.compareTo(right);
+                }
+            };
+        } catch (Exception e){
+
+        }
+    }
 }
--- securitydemo/src/test/java/lucenetest/TestFst.java
+++ securitydemo/src/test/java/lucenetest/TestFst.java
@@ -1,9 +1,43 @@
 package lucenetest;
 
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.IntsRef;
+import org.apache.lucene.util.fst.Builder;
+import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.PositiveIntOutputs;
+import org.apache.lucene.util.fst.Util;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
 /**
  * @author cuiyt
  * @Descrption
  * @create 2019/1/14
  */
 public class TestFst {
+
+    //https://www.jianshu.com/p/a7d7092fba02
+    @Test
+    public void TestFst() {
+        try {
+
+            Directory directory = FSDirectory.open(Paths.get(new File("E:\\Lucene_index").getPath()));
+
+            System.out.println("pathname"+Paths.get(new File("E:\\Lucene_index").getPath()));
+
+
+
+            SmartChineseAnalyzer smartChineseAnalyzer = new SmartChineseAnalyzer();
+
+
+
+        } catch (Exception e) {
+
+        }
+    }
 }
