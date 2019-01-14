e2a356d5d7707334ea17c7b10089a8eb32399006
cuiyt <cuiyt@jiedaibao.com>
2019-01-14 18:59:36 +0800

    es

---
+++ securitydemo/src/main/java/com/example/demo/lucenetest/TestFST.java
@@ -0,0 +1,9 @@
+package com.example.demo.lucenetest;
+
+/**
+ * @author cuiyt
+ * @Descrption
+ * @create 2019/1/14
+ */
+public class TestFST {
+}
---
+++ securitydemo/src/test/java/lucenetest/TestFst.java
@@ -0,0 +1,9 @@
+package lucenetest;
+
+/**
+ * @author cuiyt
+ * @Descrption
+ * @create 2019/1/14
+ */
+public class TestFst {
+}
